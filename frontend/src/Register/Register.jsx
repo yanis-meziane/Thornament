b0b53551a06cd79c700d415a307ec0690d329a4a
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-27 11:39:25 +0100

    Ajout d'une première sécurité pour le Register

--- frontend/src/Register/Register.jsx
+++ frontend/src/Register/Register.jsx
@@ -1,20 +1,64 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function Register(){
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(password.length < 8){
+            setError('Le mot de passe doit contenir au minimum 8 caractères.');
+            return;
+        }
+
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@_\-$!*%&]).{8,}$/;
+        if(!passwordRegex.test(password)){
+            setError('Le mot de passe est trop faible ! Il est nécessaire d\'avoir au minimum 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.');
+            return;
+        }
+
+        if(password !== confirmPassword){
+            setError('Les mots de passe ne correspondent pas.');
+            return;
+        }
+        
+        setError('');
+        alert("Inscription réussie !");
+    };
+
     return(
         <div>
             <h1>Je suis la page register</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div id="divFirstname">
                     <label htmlFor="firstname">Prénom</label>
-                    <input type="text" placeholder="Firstname..." name="lastname" id="firstname" minLength={1} maxLength={50}/>
+                    <input type="text" placeholder="Firstname..." name="firstname" id="firstname" minLength={1} maxLength={50}/>
                 </div>
 
                 <div id="divLastname">
                     <label htmlFor="lastname">Nom de famille</label>
                     <input type="text" name="lastname" id="lastname" placeholder="Lastname..." minLength={1} maxLength={30} />
                 </div>
+
+                <div id="divEmail">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Email..." minLength={5} maxLength={50} />
+                </div>
+
+                <div id="divPassword">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" placeholder="Password..." minLength={8} maxLength={20} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                <div id="divConfirmPassword">
+                    <label htmlFor="confirmPassword">Confirmer le mot de passe</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password..." minLength={8} maxLength={20} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                </div>
+                <button type="submit">S'inscrire</button>
             </form>
 
             <p>Si vous avez déjà un compte, connectez-vous <Link to={'/Login'}>ici</Link></p>
