61e2a179e87760f7c8901b0bbea5d7c7cdd59a29
Maxime <maxime.bernheim@efrei.net>
2026-01-28 15:14:19 +0100

    màj d'import

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,6 @@
 import { crypt, compare } from "../services/hash.js";
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+//import User from '../models/User.js';
 import {registerRepository, loginRepository} from '../repositories/authRepositories.js'
 
 process.loadEnvFile("./.env");
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+//import User from '../models/User.js';
 
 const getUserById = async (req, res, next) => {
   try {
