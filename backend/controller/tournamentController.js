02a6e38caaa187cc5244accc2a840113d88ca9e9
Bastien Puyou Filipic <bastien.puyou.filipic@gmail.com>
2026-01-28 14:50:19 +0100

    wip: tournament backend

---
+++ backend/controller/tournamentController.js
@@ -0,0 +1,42 @@
+// import {} from '../repositories/authRepositories.js'
+
+process.loadEnvFile("./.env");
+
+const getAllTournaments = async (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+}
+
+const createTournament = async (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+}
+
+const getTournamentById = async (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+}
+
+const deleteTournament = async (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+}
+
+export default {
+  getAllTournaments,
+  createTournament,
+  getTournamentById,
+  deleteTournament,
+}
