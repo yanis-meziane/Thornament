cf68e170f6f7f1b348cba4d560aa3430544ba817
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-30 10:01:14 +0100

    Ajout image

--- frontend/src/Navbar/Navbar.jsx
+++ frontend/src/Navbar/Navbar.jsx
@@ -1,27 +1,23 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar({ title, isConnected = false }) {
     const navigate = useNavigate();
-    const location = useLocation();
-
-    // Ne pas afficher le bouton de retour sur la page d'accueil
-    const showBackButton = location.pathname !== '/' && location.pathname !== '/tournament';
-
     return (
         <nav className="navbar">
             <div>
-                {showBackButton && (
-                    <button className="btn-back" onClick={() => navigate(-1)} title="Retour">
+                    <button onClick={() => navigate("/home")}>
                         Accueil
                     </button>
-                )}
             </div>
             
             <div>
                 <h1>{title}</h1>
             </div>
 
+            <div>
+                <img src='./public/thornament.png' alt='Logo'></img>
+            </div>
         </nav>
     );
 }
